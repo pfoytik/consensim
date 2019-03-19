253404906a52b47726ededdf91a622036a41fe9a
pfoytik <pfoytik@odu.edu>
2019-03-19 15:06:40 -0400

    initial commit with working directory

---
+++ ns-allinone-3.29/backed_ns-3.29/build/optimized/build-status.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+
+# Programs that are runnable.
+ns3_runnable_programs = ['build/optimized/scratch/ns3.29-scratch-simulator-optimized', 'build/optimized/scratch/ns3.29-bitcoin-test-optimized', 'build/optimized/scratch/subdir/ns3.29-subdir-optimized', 'build/optimized/scratch/ns3.29-selfish-miner-test-optimized']
+
+# Scripts that are runnable.
+ns3_runnable_scripts = []
+
